0aec2cf27cc18ca472ab679a9a899f55370756ff
hnjilee <hnjilee@gmail.com>
2022-07-21 11:10:34 +0900

    Handle words check

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 'use strict';
 
+const checked = [];
+
 loadWords() //
   .then(words => {
     const wordListItems = displayWords(words);
+
+    // Add a checked word to the 'checked' array
+    // Remove a unchecked one from the array
+    const wordsList = document.querySelector('.words');
+    wordsList.addEventListener('change', e =>
+      onCheckboxChange(e, wordListItems)
+    );
   })
   .catch(console.log);
 
@@ -28,11 +37,33 @@ function createListItem(word) {
   listItem.classList.add('word');
   listItem.setAttribute('data-id', word.id);
   listItem.innerHTML = `
-    <input type="checkbox" class="word__checkbox" />
+    <input type="checkbox" class="word__checkbox" data-id="${word.id}"/>
     <div class="word__content">
       <span>${word.eng}</span>
       <span>${word.kor}</span>
     </div>
     `;
   return listItem;
+}
+
+function onCheckboxChange(e, words) {
+  const target = e.target;
+  const id = target.dataset.id;
+  const targetWord = words.filter(word => word.dataset.id === id)[0];
+
+  if (target.checked) {
+    onCheck(targetWord);
+  } else {
+    onUncheck(targetWord);
+  }
+}
+
+function onCheck(word) {
+  checked.push(word);
+  checked.sort((a, b) => a.dataset.id - b.dataset.id);
+}
+
+function onUncheck(word) {
+  const index = checked.indexOf(word);
+  checked.splice(index, 1);
 }
